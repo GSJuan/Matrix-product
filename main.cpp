7881c3b72211a4f28041f025d09e7c4a7c1fd916
JuanGarciaSantos <alu0101325583@ull.edu.es>
2022-02-26 15:12:35 +0000

    added time recording and auto csv formatting output

--- main.cpp
+++ main.cpp
@@ -7,40 +7,42 @@ int main( void ) {
 time_t t;
 srand((unsigned) time(&t));
 
-std::ofstream csv;
-csv.open ("comparative.csv");
-csv << "Tamaño,Row Product Time microseconds,Row column Time microseconds\n"; //columnas del csv
-int n = 10;
+std::ofstream csv; 
+csv.open ("comparative.csv"); //abrimos o creamos el archivo.csv
+csv << "Tamaño,Row Product Time microseconds,Column Product Time microseconds\n"; //columnas del csv
+
+int n = 10; //inicializamos el numero de filas (n) y columnas(m)
 int m = 10;
 
-while(n < 2010) {
+while(n < 2010) { // 2010 es el valor que ha mi me ha parecido a partir del cual va demasiado lento
     n += 100;
     m += 100;
     Matrix matriz1(n, m);
     Matrix matriz2(n, m);
 
-    cout << "Multiplying matrices of dimensions " << n << "*" << m << " : " << endl;
+    cout << "Multiplying matrices of dimensions " << n << "*" << m << " : " << endl; 
 
     matriz1.setStrategy(new RowProduct());
     matriz2.setStrategy(new RowProduct());
 
-    auto start1 = chrono::high_resolution_clock::now(); 
-    Matrix matriz3 = matriz1 * matriz2;
-    auto stop1 = chrono::high_resolution_clock::now();
-    auto duration1 = chrono::duration_cast<chrono::microseconds>(stop1 - start1);
+    auto start1 = chrono::high_resolution_clock::now(); //empezamos el chrono
+    Matrix matriz3 = matriz1 * matriz2; //hacemos el producto
+    auto stop1 = chrono::high_resolution_clock::now(); //lo paramos
+    auto duration1 = chrono::duration_cast<chrono::microseconds>(stop1 - start1); //calculamos lo que ha tardado en micro segundos
     cout << "\tWith Row Product: " << duration1.count() << " microseconds" << endl;
 
     matriz1.setStrategy(new ColProduct());
     matriz2.setStrategy(new ColProduct());
 
-    auto start2 = chrono::high_resolution_clock::now(); 
-    Matrix matriz4 = matriz1 * matriz2;
-    auto stop2 = chrono::high_resolution_clock::now();
+    auto start2 = chrono::high_resolution_clock::now(); //empezamos el chrono
+    Matrix matriz4 = matriz1 * matriz2; //hacemos el producto
+    auto stop2 = chrono::high_resolution_clock::now(); //lo paramos
     auto duration2 = chrono::duration_cast<chrono::microseconds>(stop2 - start2);
-    cout << "\tWith Column Product: " << duration2.count() << " microseconds" << endl;
+    cout << "\tWith Column Product: " << duration2.count() << " microseconds" << endl; //calculamos lo que ha tardado en micro segundos
 
     csv << n << "x" << m << "," << duration1.count() << "," << duration2.count() << "\n";
 }
-csv.close();
+
+csv.close(); //cerramos el archivos
 return 0;
 };
